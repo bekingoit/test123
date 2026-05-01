22092e3f77ee01681e203b99d416faa745c382c7
Burak Ekin <bekin73@gmail.com>
2026-05-01 19:41:21 +0300

    test1

--- vite.config.js
+++ vite.config.js
@@ -38,7 +38,7 @@ export default defineConfig(({ command }) => {
           },
         },
       },
-      outDir: './dist',
+      outDir: '../dist',
       emptyOutDir: true,
     },
     plugins: [
